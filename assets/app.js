55b226072d9f48b21329b54e9f2136f912736061
miltongiovanni <miltongiovanni@gmail.com>
2025-11-06 22:51:08 -0500

    assets

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,18 @@
-import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './css/vendors/font-awesome.css';
+import './css/vendors/flag-icon.css';
+import './css/vendors/icofont.css';
+import './css/vendors/prism.css';
+import './css/vendors/chartist.css';
+import './css/vendors/bootstrap.css';
+import './css/style.css';
+//import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
