8cbd4abbcbd9e79330b9ec599e73425d9c9bf11d
miltongiovanni <miltongiovanni@gmail.com>
2025-11-11 22:15:00 -0500

    fix scss

--- config/bundles.php
+++ config/bundles.php
@@ -11,7 +11,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
 ];
